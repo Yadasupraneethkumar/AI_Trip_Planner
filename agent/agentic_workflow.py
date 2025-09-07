78e5cd8a6e2d6fff8f6244ba90365e49c8dcd106
PraneethKumar <yadasupraneeth@gmail.com>
2025-09-07 11:31:43 +0530

    code updated

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -1,6 +1,6 @@
 from utils.model_loader import ModelLoader
-from prompt_library.prompt import SYATEM_PROMPT
-from langgraph.graph import Stategraph, MessagesState, END, STARTd
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, END, START
 from langgraph.prebuilt import ToolNode, tools_condition
 
 # from tools.weather_info_tool import WeatherInfoTool
@@ -12,26 +12,40 @@ from langgraph.prebuilt import ToolNode, tools_condition
 
 class GraphBuilder():
     def __init__(self):
-        pass 
+        self.tools=[
+            # WeatherInfoTool(),
+            # PlaceSearchTool(),
+            # CalculatorTool(),
+            # CurrencyConvertorTool()
 
+        ]
+        self.system_prompt=SYSTEM_PROMPT
 
 
     # Tabnine|Edit|Test|Explain|Document
-    def agent_function(self):
-        pass
+    def agent_function(self, state : MessagesState):
+        """Main agent function"""
+        user_question = state["messages"]
+        input_question = [self.system_prompt] + user_question
+        response = self.llm_with_tools.invoke(input_question)
+        return {"messages" : {response}}
 
 
     # Tabnine|Edit|Test|Explain|Document
     def build_graph(self):
-        pass
+        graph_builder=StateGraph(MessagesState)
+        graph_builder.add_node("agent",self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tool=self.tools))
+        graph_builder.add_edge(START,"agent")
+        graph_builder.add_conditional_edges("agent", tools_condition)
+        graph_builder.add_edge("tools","agent")
+        graph_builder.add_edge("agent", END)
+
+        self.graph = graph_builder.compile()
+        return self.graph
 
 
 
     # Tabnine|Edit|Test|Explain|Document
     def __call__(self):
-        pass
-
-
-    # Tabnine|Edit|Test|Explain|Document
-    def add_note(self,node):
-        pass
+        return self.build_graph()
